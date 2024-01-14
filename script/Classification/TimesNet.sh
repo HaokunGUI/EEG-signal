95309c22beff603cd5968a94318229689f403dff
ChrisGuiHK <200110701@stu.hit.edu.cn>
2024-01-14 22:26:29 +0800

    fix bugs in classification

--- script/Classification/TimesNet.sh
+++ script/Classification/TimesNet.sh
@@ -10,11 +10,12 @@ torchrun \
     --num_kernels 6 \
     --d_model 16 \
     --e_layers 3 \
-    --dropout 0.2 \
+    --dropout 0.4 \
     --top_k 3 \
     --num_epochs 60 \
     --train_batch_size 64 \
     --test_batch_size 64 \
     --use_scheduler \
     --num_workers 8 \
-    --normalize
+    --normalize \
+    --input_len 12
